b66c09c2ab8b524ae8c4937e98881ea7751a90cd
Andrii Pivovarov <Andro999b@gmail.com>
2020-05-12 15:49:59 +0300

    search api test

---
+++ test/search.test.js
@@ -0,0 +1,54 @@
+const axios = require("axios")
+const { expect } = require("chai")
+
+const baseApiUrl = "https://6gov3btrq2.execute-api.eu-central-1.amazonaws.com/dev/api/trackers"
+
+const testParameters = [
+    {
+        provider: "nekomori",
+        query: "Тетрадь смерти"
+    },   
+    {
+        provider: "animevost",
+        query: "Тетрадь смерти"
+    },
+    {
+        provider: "seasonvar",
+        query: "Во все тяжкие"
+    },
+    {
+        provider: "kinogo",
+        query: "Терминатор"
+    },
+    {
+        provider: "videocdn",
+        query: "Терминатор"
+    },
+    {
+        provider: "kinovod",
+        query: "Терминатор"
+    },
+    {
+        provider: "7serealov",
+        query: "Во все тяжкие"
+    }
+]
+
+describe("SearchAPI", () => {
+    testParameters.forEach(({ provider, query }) => {
+        it(`Provider ${provider} should return results by search query '${query}'`, async () => {
+            const res =  await axios.get(`${baseApiUrl}/${provider}/search?q=${encodeURIComponent(query)}`)
+            const { data, status } = res
+
+            expect(status, `Server respond status: ${status}`).to.equal(200)
+            expect(data, `Empty response`).to.be.not.empty
+            
+            data.forEach((result) => {
+                expect(result.provider, "Missing correct provider value in result").to.be.equal(provider)
+                expect(result.name, `Empty name`).to.be.not.empty
+                expect(result.image, `Empty image`).to.be.not.empty
+                expect(result.id, `Missing id`).to.be.not.null
+            })
+        })
+    })
+})
